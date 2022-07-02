5292884533d92b274611231bf0bb9da098e1d2df
lia-arroyo <liayzabel@gmail.com>
2022-07-02 20:25:21 +1200

    added intro text and started styling landing page

--- src/screens/index/Landing.js
+++ src/screens/index/Landing.js
@@ -3,7 +3,11 @@ import React from "react";
 function Landing() {
   return (
     <div className="landing">
-      <img className="circleImage" id="landingHeadshot" src="/media/liaSquare.png" alt="A headshot photo of Lia Arroyo"></img>
+      <img className="circleImage" src="/media/liaSquare.png" alt="A headshot photo of Lia Arroyo"></img>
+
+      <div id="welcomeText">
+        <h1>Hi! I'm <a id="name" href="#">Lia Arroyo</a>.</h1>
+      </div>
     </div>
   );
 }
