8932dd2897c05a2c448eb415d585def246241047
lia-arroyo <liayzabel@gmail.com>
2022-07-02 19:14:41 +1200

    reorganised folders, and added headshot image to landing page

---
+++ src/screens/index/Landing.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Landing() {
+  return (
+    <div className="landing">
+      <img className="circleImage" id="landingHeadshot" src="/media/liaSquare.png" alt="A headshot photo of Lia Arroyo"></img>
+    </div>
+  );
+}
+
+export default Landing;
