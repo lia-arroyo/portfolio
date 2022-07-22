1f20cef08cf13ad75d2f088149be95dc484acbcb
lia-arroyo <liayzabel@gmail.com>
2022-07-22 21:15:21 +1200

    testing pr

--- src/screens/index/Landing.js
+++ src/screens/index/Landing.js
@@ -6,7 +6,7 @@ function Landing() {
       <img className="circleImage" src="/media/liaSquare.png" alt="A headshot photo of Lia Arroyo"></img>
 
       <div id="welcomeText">
-        <h1>Hi! I'm <a id="name" href="#">Lia Arroyo</a>.</h1>
+        <h1>Hi! I'm <a id="name" href="#">Lia Arroyoooo</a>.</h1>
         <h2>An impact-driven Software Engineering student with a Marketing mindset and experience coding in a team environment, looking to grow my dev skills!</h2>
         <a className="button" href="https://www.linkedin.com/in/liayzabel/" target="_BLANK">Learn more</a>
       </div>
